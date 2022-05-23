064d11fb6874a57e61f9530bbd08ee828aa14359
Juan Cruz Gutierrez <juan.cruz.gutierrez.saravia@gmail.com>
2022-05-23 00:38:55 -0300

    Primer cambio en landing. Agrego svg dummy para probar las animaciones en el carousel. Estructura base del modelo grid

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,20 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import { LandingCarousel } from './LandingCarousel'
+import '../../styles/landing.css';
+
 export const Landing = () => {
     return(
-        <>
-        <h1>Soy una landing page</h1>
-        </>
+        <Container fluid>
+            <Row className='backgroundCarousel'>
+                <Col className='col-12'>
+                    <LandingCarousel />
+                </Col>
+                <Col className='col-12 text-center'>
+                    <div className='d-flex justify-content-center'>
+                        <h1 style={{width:"70%", color:"white"}} className="ps-5 pe-5 pb-5 belowGreen">La aplicacion web que te permite ordenar, controlar y agilizar el procesamiento de turnos</h1>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
---
+++ src/components/Landing/LandingCarousel.js
@@ -0,0 +1,27 @@
+import Landing_1 from '../../img/Landing_1.svg';
+import Landing_2 from '../../img/Landing_2.svg';
+import Landing_3 from '../../img/Landing_3.svg';
+import '../../styles/landing.css';
+import { Carousel } from 'react-bootstrap';
+
+export const LandingCarousel = () =>{
+    return(
+        <Carousel indicators={false}>
+            <Carousel.Item interval={40000}>
+                <div className='text-center'>
+                    <img alt="landingDecoration" className="img-fluid-max sizeImgLanding" src={Landing_1} />
+                </div>
+            </Carousel.Item>
+            <Carousel.Item interval={4000}>
+                <div className='text-center'>
+                    <img alt="landingDecoration" className="img-fluid-max sizeImgLanding" src={Landing_2} />
+                </div>
+            </Carousel.Item>
+            <Carousel.Item interval={4000}>
+                <div className='text-center'>
+                    <img alt="landingDecoration" className="img-fluid-max sizeImgLanding" src={Landing_3} />
+                </div>
+            </Carousel.Item>
+        </Carousel>
+    )
+}
