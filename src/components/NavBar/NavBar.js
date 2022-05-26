dd0dfc095dab625a254e16e41497b0c1b1e2ace7
Juan Cruz Gutierrez <juan.cruz.gutierrez.saravia@gmail.com>
2022-05-25 23:07:49 -0300

    Creacion de funcion que renderiza componente para navbar en usuario logueado

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,29 +1,62 @@
 import { Navbar, Container, Nav, NavItem, NavLink } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { Logo } from '../Logo/Logo'
+import { useAuth } from "../../providers/useAuth"
 
 export const NavBar = () => {
-    return (
-         <Navbar bg="dark" variant="dark" expand="md">
-            <Container fluid>
-                <Navbar.Brand>
-                    <Link className="navbar-brand" to="/" style={{ color: "white", textDecoration: "none" }}><Logo />
-                    {' '}
-                    Vacunassist
-                    </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <NavItem>
-                            <Link to="/" className="nav-link">Home</Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link to="/login" className="nav-link">Iniciar Sesion</Link>
-                        </NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+    const auth = useAuth();
+
+    const NavBarNotLoggedIn = () =>{
+        return (
+            <Navbar bg="dark" variant="dark" expand="md">
+               <Container fluid>
+                   <Navbar.Brand>
+                       <Link className="navbar-brand" to="/protected" style={{ color: "white", textDecoration: "none" }}><Logo />
+                       {' '}
+                       Vacunassist
+                       </Link>
+                   </Navbar.Brand>
+                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                   <Navbar.Collapse id="basic-navbar-nav">
+                       <Nav className="me-auto">
+                           <NavItem>
+                               <Link to="/protected" className="nav-link">Home</Link>
+                           </NavItem>
+                           <NavItem>
+                               <Link to="/protected" className="nav-link">Mis vacunas</Link>
+                           </NavItem>
+                       </Nav>
+                   </Navbar.Collapse>
+               </Container>
+           </Navbar>
+        )
+    }
+
+    const NavBarLoggedIn = () => {
+        return (
+            <Navbar bg="dark" variant="dark" expand="md">
+               <Container fluid>
+                   <Navbar.Brand>
+                       <Link className="navbar-brand" to="/" style={{ color: "white", textDecoration: "none" }}><Logo />
+                       {' '}
+                       Vacunassist
+                       </Link>
+                   </Navbar.Brand>
+                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                   <Navbar.Collapse id="basic-navbar-nav">
+                       <Nav className="me-auto">
+                           <NavItem>
+                               <Link to="/" className="nav-link">Home</Link>
+                           </NavItem>
+                           <NavItem>
+                               <Link to="/login" className="nav-link">Iniciar Sesion</Link>
+                           </NavItem>
+                       </Nav>
+                   </Navbar.Collapse>
+               </Container>
+           </Navbar>
+       )
+    }
+   
+    return auth.user ? <NavBarNotLoggedIn/> : <NavBarLoggedIn/>
 }
