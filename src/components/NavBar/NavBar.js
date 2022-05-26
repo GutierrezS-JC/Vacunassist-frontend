905cfd3ca5dcd9a0d6f19d93e084096d5f96885f
Juan Cruz Gutierrez <juan.cruz.gutierrez.saravia@gmail.com>
2022-05-26 01:45:22 -0300

    Cambios en navbar para usuario logueado. Responsive display

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,51 @@
-import { Navbar, Container, Nav, NavItem, NavLink } from "react-bootstrap"
+import '../../styles/navbar.css';
+import { Navbar, Container, Nav, NavItem, NavLink, Button, Dropdown } from "react-bootstrap"
+import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom"
 import { Logo } from '../Logo/Logo'
 import { useAuth } from "../../providers/useAuth"
+import withReactContent from 'sweetalert2-react-content'
+import Swal from 'sweetalert2'
 
 export const NavBar = () => {
     const auth = useAuth();
+    const navigate = useNavigate();
+    const MySwal = withReactContent(Swal);
 
-    const NavBarNotLoggedIn = () =>{
+    const logoutAlert = () => {
+        MySwal.fire({
+            position: 'top-end',
+            title: 'Sesion cerrada',
+            showConfirmButton: false,
+            icon: 'success',
+            timer:1500
+        })
+    }
+
+    const handleLogout = () => {
+        setTimeout(() => {
+            auth.logout();
+            logoutAlert()
+            navigate('/login')
+        }, 500)
+    }
+
+    const UserCircle = () => {
+        return (
+            <Dropdown drop='start'>
+                <Dropdown.Toggle className='circle rounded-circle caret-off' variant="success" id="dropdown-basic">
+                    {auth.user.email[0].toUpperCase()}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item>Mi perfil</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item onClick={ ()=> handleLogout() }>Cerrar Sesion</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        )
+    }
+
+    const NavBarLoggedIn = () =>{
         return (
             <Navbar bg="dark" variant="dark" expand="md">
                <Container fluid>
@@ -17,14 +56,23 @@ export const NavBar = () => {
                        </Link>
                    </Navbar.Brand>
                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                   <Navbar.Collapse id="basic-navbar-nav">
+                   <Navbar.Collapse id="basic-navbar-nav" >
                        <Nav className="me-auto">
-                           <NavItem>
-                               <Link to="/protected" className="nav-link">Home</Link>
-                           </NavItem>
                            <NavItem>
                                <Link to="/protected" className="nav-link">Mis vacunas</Link>
                            </NavItem>
+                           <NavItem>
+                               <Link to="/protected" className="nav-link">Mis turnos</Link>
+                           </NavItem>
+                           <NavItem>
+                               <Link to="/protected" className="nav-link d-sm-block d-md-none">Mi Perfil</Link>
+                           </NavItem>
+                           <NavItem>
+                               <Nav.Link onClick={ ()=> handleLogout() } className="nav-link d-sm-block d-md-none">Cerrar sesion</Nav.Link>
+                           </NavItem>
+                        </Nav>
+                        <Nav className="justify-content-end d-none d-md-block">
+                           <NavItem><UserCircle/></NavItem>
                        </Nav>
                    </Navbar.Collapse>
                </Container>
@@ -32,7 +80,7 @@ export const NavBar = () => {
         )
     }
 
-    const NavBarLoggedIn = () => {
+    const NavBarNotLoggedIn = () => {
         return (
             <Navbar bg="dark" variant="dark" expand="md">
                <Container fluid>
@@ -48,9 +96,11 @@ export const NavBar = () => {
                            <NavItem>
                                <Link to="/" className="nav-link">Home</Link>
                            </NavItem>
-                           <NavItem>
-                               <Link to="/login" className="nav-link">Iniciar Sesion</Link>
-                           </NavItem>
+                        </Nav>
+                        <Nav>
+                            <NavItem>
+                                <Button onClick={() => navigate("/login")} variant="outline-success">Iniciar Sesion</Button>
+                            </NavItem>
                        </Nav>
                    </Navbar.Collapse>
                </Container>
@@ -58,5 +108,5 @@ export const NavBar = () => {
        )
     }
    
-    return auth.user ? <NavBarNotLoggedIn/> : <NavBarLoggedIn/>
+    return auth.user ? <NavBarLoggedIn/> : <NavBarNotLoggedIn/>
 }
