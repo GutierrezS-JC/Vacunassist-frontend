83941fb10007e2bf00bcf0d727547edb30a1f4ad
Maria Rosa Marcheschi <romarcheschi@gmail.com>
2022-05-26 01:45:10 -0300

    Cambios en login
    .Cambie color fondo y agregue pattern con +
    .Cambie(agregue 2 a img) imagenes del corousel y  texto de bajo de cada imagen
    .Agregue un box blanco para el form

--- src/components/Login/LoginCarousel.js
+++ src/components/Login/LoginCarousel.js
@@ -1,7 +1,9 @@
 import '../../styles/login.css';
 import LoginUndraw2 from '../../img/LoginUndraw2.svg';
+import LoginUndraw1 from '../../img/LoginUndraw1.svg';
+import LoginUndraw3 from '../../img/LoginUndraw3.svg';
 import { Carousel } from 'react-bootstrap';
-
+/*Ro, cambie imagenes del carousel y texto de abajo */
 export const LoginCarousel = () =>{
     return(
         <Carousel indicators={true} controls={false}>
@@ -11,27 +13,27 @@ export const LoginCarousel = () =>{
                 </div>
                 <div>
                 <Carousel.Caption>
-                    <h3>Solicita un turno</h3>
-                    <p>O no...</p>
+                    <h3>Registrate</h3>
+                    <p>O inicia sesion</p>
                 </Carousel.Caption>
                 </div>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
                 <div>
-                    <img alt="loginDecoration" className="img-fluid-max ms-4 ms-lg-4 ms-xl-5 sizeImg" src={LoginUndraw2} />
+                    <img alt="loginDecoration" className="img-fluid-max ms-4 ms-lg-4 ms-xl-5 sizeImg" src={LoginUndraw1} />
                 </div>
                 <Carousel.Caption>
-                    <h3>Administra tus vacunas</h3>
-                    <p>Si queres...</p>
+                    <h3>Pedi tus turnos</h3>
+                    <p>desde la comodidad de tu casa?</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
                 <div>
-                    <img alt="loginDecoration" className="img-fluid-max ms-4 ms-lg-4 ms-xl-5 sizeImg" src={LoginUndraw2} />
+                    <img alt="loginDecoration" className="img-fluid-max ms-4 ms-lg-4 ms-xl-5 sizeImg" src={LoginUndraw3} />
                 </div>
                 <Carousel.Caption>
-                    <h3>Siempre disponibles</h3>
-                    <p>Ponele...</p>
+                    <h3>Veni a vacunarte!</h3>
+                    <p>Te esperamos...</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
