ffc710d9321bf938b098ba91d8f34407265da1d2
Dolo Pondarre <dolopondarre@icloud.com>
2022-05-31 21:30:24 -0300

    Avance modifNomVacunat

--- src/components/ModifNomVaunat/ModifNomVacunat.js
+++ src/components/ModifNomVaunat/ModifNomVacunat.js
@@ -1,17 +1,20 @@
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import Dummy_Register_Vac from '../../img/Dummy_Register_Vac.svg';
+import {useState} from 'react';
 
-document.addEventListener("DOMContentLoaded", function() {
+{/*document.addEventListener("DOMContentLoaded", function() {
     console.log('asd');
     let formulario = document.getElementById("formNameVacun");
     console.log('asd');
-    formulario.addEventListener("submit", validarFormulario); 
+    formulario.addEventListener("submit", validarFormulario2); 
     console.log('asd');
 });
+*/}
   
-function validarFormulario(evento) {
+{/*function validarFormulario(evento) {
     evento.preventDefault();
-    console.log('asd');
+    console.log(evento.name);
+    console.log(evento.value);
     var i = document.formNameSelect.options.length;
     var indice = document.getElementById('options').selectedIndex;
     if(indice == null || indice === 0) {
@@ -32,26 +35,74 @@ function validarFormulario(evento) {
     }
     this.submit();
 }
+*/}
+
 
 export const ModifNomVacunat = () => {
+    const [ errors, setErrors ] = useState({})
+    const [ options, setOptions ] = useState()
+    const [ nameVacun, setNameVacun ] = useState()
+
+    const [vacunatorioForm, setVacunatorioForm] = useState({
+        options: '',
+        nameVacun: ''
+    })
+
+    {/*const handleChange = (event) => {
+        console.log(event.target.value)
+        console.log(event.target.name)
+        setVacunatorioForm({ ...vacunatorioForm, [event.target.name]: event.target.value });
+        
+        //new
+        if(!!errors[event.target.name]) setErrors({
+            ...errors,
+            [event.target.name]: null
+        })
+    };
+*/}
+
+
+    function validarFormulario2(evento) {
+        evento.preventDefault();
+        console.log(evento.name);
+        console.log(evento.value);
+        var indice = document.getElementById('options').selectedIndex;
+        if(indice == null || indice === 0) {
+            alert('Debe seleccionar un vacunatorio');
+            return;
+        }
+        var formNameChange = document.getElementById('formNameChange').value;
+        if(formNameChange.length === 0) {
+            alert('Debe ingresar un nuevo nombre');
+            return;
+        }
+        
+        this.submit();
+    }
+
+    const validar = (e) => {
+        e.preventDefault();
+        setNameVacun(e.target.value);
+    }
+
     const Opciones = () => {
         return(
-            <Form style={{}} id="formNameVacun">
+            <Form noValidate onSubmit={validarFormulario2}>
                 <Form.Group className="mb-3" controlId="formNameSelect">
                     <Form.Label>Vacunatorio</Form.Label>
-                    <Form.Select id="options" name="options">
+                    <Form.Select name="options" value={options} onChange={(e) => setOptions(e.target.value)}>
                         <option vuale="">- Seleccione un vacunatorio -</option>
                         <option value="1">Centro</option>
                         <option value="2">Terminal</option>
                         <option value="3">Cementerio</option>
                     </Form.Select>
                 </Form.Group>
-                <Form.Group className="mb-3" style={{}} controlId="formNameChange">
+                <Form.Group className="mb-3">
                     <Form.Label>Nuevo nombre</Form.Label>
-                    <Form.Control type="text" placeholder="Ingresa un nuevo nombre"/>
+                    <Form.Control type="text" name="nameVacun" placeholder="Ingresa un nuevo nombre" value={nameVacun} onChange={validar}/>
                 </Form.Group>       
 
-                <Button className="mt-2" variant="success" type="submit" id="submit">
+                <Button className="mt-2" variant="success" id="submit">
                     Aceptar
                 </Button>
 
