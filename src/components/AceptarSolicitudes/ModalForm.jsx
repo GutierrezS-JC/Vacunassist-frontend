a24631b1951d169de6ebb5fe7242d9bcec41b7a1
Juan Cruz Gutierrez <juan.cruz.gutierrez.saravia@gmail.com>
2022-06-27 18:39:16 -0300

    Se ignoran los turnos ya otorgados dentro del datepicker

--- src/components/AceptarSolicitudes/ModalForm.jsx
+++ src/components/AceptarSolicitudes/ModalForm.jsx
@@ -1,21 +1,44 @@
 import {Button, Modal, Form} from "react-bootstrap";
-import { useState } from "react";
-import { setHours, setMinutes } from "date-fns";
+import { useState, useEffect } from "react";
+import { setHours, setMinutes, parse } from "date-fns";
 import DatePicker from "react-datepicker";
+import axios from "axios";
 import "react-datepicker/dist/react-datepicker.css";
 import '../../styles/listaSolicitudes.css'
+import { setSeconds } from "date-fns";
 
 export const ModalForm = ({handleClose, show}) => {
+    const { format } = require("date-fns");
+    const [ horasConTurno, setHorasConTurno ] = useState();
+    const [ mounted, setMounted ] = useState();
     const [ turnoForm, setTurnoForm] = useState({
         pacienteId:'',
         adminId: '',
-        fechaTurno: new Date()
+        fechaTurno: new Date(),
+        
     })
+
+    useEffect(()=>{
+        const fetchTurnosDia = async () => {
+          try{
+              const response = await axios.get(`http://localhost:8080/getTurnosHorasAsignadosEnFecha?fecha=${format(new Date(turnoForm.fechaTurno),"yyyy-MM-dd")}`);
+              console.log(response.data)
+              setHorasConTurno(response.data)
+              setMounted(true)
+          }
+          catch(e){
+            console.log(e.stack)
+          }
+        }
+
+    fetchTurnosDia();
+    },[turnoForm.fechaTurno])
 
     const handleFecha = (event) => {
         console.log(event)
         setTurnoForm({...turnoForm, ["fechaTurno"] : event})
     }
+
     return (
         <>        
           <Modal show={show} onHide={handleClose}>
@@ -33,6 +56,15 @@ export const ModalForm = ({handleClose, show}) => {
                     onChange={(date) => handleFecha(date)}
                     locale="es"
                     showTimeSelect
+                    // excludeTimes={[setHours(setMinutes(turnoForm.fechaTurno, '00'), '10')]}
+                    excludeTimes={mounted ? new Object(horasConTurno.map((hora)=>{
+                      let splitted = hora.split(':');
+                      console.log(splitted)
+                      console.log(turnoForm)
+                      return(
+                        setHours(setMinutes(turnoForm.fechaTurno.setSeconds(0), +splitted[1]), +splitted[0])
+                      )
+                    })): []}
                     timeIntervals={15}
                     minTime={setHours(setMinutes(new Date(), 0), 10)}
                     maxTime={setHours(setMinutes(new Date(), 0), 17)}
