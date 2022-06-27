41a94769ad6ad825ed1dd06ed2f90df3fa9d015e
Juan Cruz Gutierrez <juan.cruz.gutierrez.saravia@gmail.com>
2022-06-26 21:23:09 -0300

    Listado fiebre amarilla v2 sin Table porque complicaba el manejo de modal

--- src/components/AceptarSolicitudes/ListadoSolicitudes.js
+++ src/components/AceptarSolicitudes/ListadoSolicitudes.js
@@ -1,21 +1,20 @@
-import {Table, Button, ButtonGroup} from "react-bootstrap";
+import {Table, Button, ButtonGroup, Row, Col, Container} from "react-bootstrap";
 
-export const ListadoSolicitudes = ({solicitudes}) => {
-   const SolicitudesRender = () =>{
+export const ListadoSolicitudes = ({solicitudes, eliminarSolicitud}) => {
+   
+    const SolicitudesRender = () =>{
         return(
             <>
                 {solicitudes.map((solicitud,index) => {
                     return(
                             <tr key={`tr${solicitud.id}`}>
-                                <td key={`Id${solicitud.id}`}>{solicitud.pacienteId}</td>
                                 <td key={`Nombre ${solicitud.id}`}>{solicitud.pacienteNombre}</td>
                                 <td key={`Apellido ${solicitud.id}`}>{solicitud.pacienteApellido}</td>
                                 <td key={`DNI ${solicitud.id}`}>{solicitud.pacienteDni}</td>
                                 <td key={`Fecha solicitud ${solicitud.id}`}>{solicitud.fechaSolicitud}</td>
-                                <td key={`Fecha actualizacion ${solicitud.id}`}>{solicitud.fechaActualizacion}</td>
-                                <ButtonGroup role="group" aria-label="Basic mixed styles example">
-                                    <Button type="button" className="btn btn-danger" disabled>Rechazar</Button>
-                                    <Button type="button" className="btn btn-success" disabled>Aceptar</Button>
+                                <ButtonGroup role="group">
+                                    <Button variant="outline-success">Aceptar</Button>
+                                    <Button variant="outline-danger">Rechazar</Button>
                                 </ButtonGroup>
                             </tr>
                     )
@@ -27,28 +26,114 @@ export const ListadoSolicitudes = ({solicitudes}) => {
 
     const TableSolicitudes = () =>{
         return(
-            <Table striped bordered hover responsive="lg">
+            <Table  hover responsive="lg">
                 <thead>
                     <tr>
-                        <th>Id paciente</th>
                         <th>Nombre</th>
                         <th>Apellido</th>
                         <th>DNI</th>
                         <th>Fecha solicitud</th>
-                        <th>Fecha actualizacion</th>
-                        <th>Estado solicitud</th>
+                        <th>Opciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <SolicitudesRender/>
+                    {/* <SolicitudesRender/> */}
                 </tbody>
             </Table>
         )
     }
 
+    const SolicitudesRenderV2 = () =>{
+        return(
+            <>
+                {solicitudes.map((solicitud, index) => {
+                    return(
+                        <div key={`div ${index}`}>
+                            <div className="d-none d-md-block">
+                                <Row className="mt-4">
+                                <Col className="col-md-1 col-12 d-flex" >
+                                        <p className="justify-content-center align-self-center  ml-md-4"><strong>{solicitud.id}</strong></p>
+                                    </Col>
+                                    <Col className="col-md-2 col-12 d-flex" >
+                                        <p className="justify-content-center align-self-center  ml-md-4">{solicitud.pacienteNombre}</p>
+                                    </Col>
+                                    <Col className="col-md-2 col-12 d-flex " >
+                                        <p className="justify-content-center align-self-center ml-md-4">{solicitud.pacienteApellido}</p>
+                                    </Col>
+                                    <Col className="col-md-2 col-12 d-flex"  >
+                                        <p className="justify-content-center align-self-center ml-md-3" >{solicitud.pacienteDni}</p>
+                                    </Col>
+                                    <Col className="col-md-2 col-12 d-flex"  >
+                                        <p className="justify-content-center align-self-center ml-md-3" >{solicitud.fechaSolicitud}</p>
+                                    </Col>
+                                    <Col className="col-md-3 col-12 d-flex">
+                                        <Button className="justify-content-center align-self-center me-2" variant="outline-success">Aceptar</Button>
+                                        <Button className="justify-content-center align-self-center" variant="outline-danger" onClick={(e)=> eliminarSolicitud()}>Rechazar</Button>
+                                    </Col>
+                                </Row>
+                                <hr key={`hr ${index}`} />
+                            </div>
+                     
+
+                            <div className="d-block d-md-none">
+                                <Row className="mt-4">
+                                    <Col className="col-sm-12">
+                                        <Col className="col-sm-12" >
+                                            <b>ID:</b> {solicitud.id}
+                                            <br/>
+                                            <b>Nombre completo:</b> {solicitud.pacienteNombre} {solicitud.pacienteApellido}
+                                            <br/>
+                                            <b>DNI:</b> {solicitud.pacienteDni}
+                                            <br/>
+                                            <b>Fecha Solicitud</b> {solicitud.fechaSolicitud}
+                                        </Col>
+                                        <Col className="col-sm-12 mt-3">
+                                            <Button className="justify-content-center align-self-center me-2" variant="outline-success">Aceptar</Button>
+                                            <Button className="justify-content-center align-self-center" variant="outline-danger" onClick={(e)=> eliminarSolicitud()}>Rechazar</Button>
+                                        </Col>
+                                    </Col>
+                                </Row>
+                                <hr />
+                            </div>
+                        </div>
+                    )
+                })}
+            </>
+        )
+    }
+
+    const TableSolicitudesV2 = () => {
+        return(
+            <Container style={{width:"90%"}}>
+                <div className="d-none d-md-block">
+                    <Row className="mt-4">
+                        <Col className="col-1">
+                            <h6><strong>#</strong></h6>
+                        </Col>
+                        <Col className="col-2">
+                            <h6><strong>Nombre</strong></h6>
+                        </Col>
+                        <Col className="col-2">
+                            <h6><strong>Apellido</strong></h6>
+                        </Col>
+                        <Col className="col-2">
+                            <h6><strong>DNI</strong></h6>
+                        </Col>
+                        <Col className="col-2">
+                            <h6><strong>Fecha Solicitud</strong></h6>
+                        </Col>
+                
+                    </Row>
+                </div>
+                <hr style={{ height: "1px", border: "none", color: "#333", backgroundColor: "#333" }} />
+                <SolicitudesRenderV2/>
+            </Container>
+        )
+    }
+
     return(
         <>
-           {solicitudes.length !== 0 ? <TableSolicitudes/>  : <h3> No hay solicitudes para la vacuna de la Fiebre Amarilla </h3>}
+           {solicitudes.length !== 0 ? <TableSolicitudesV2/>  : <h3> No hay solicitudes para la vacuna de la Fiebre Amarilla </h3>}
         </>
     )
 }
--- src/containers/SolicitarSolicitudesContainer/SolicitudesContainer.js
+++ src/containers/SolicitarSolicitudesContainer/SolicitudesContainer.js
@@ -3,55 +3,57 @@ import { useEffect, useState } from "react";
 import { Container, Row, Col, Form, Button, Table } from "react-bootstrap";
 import { ListadoSolicitudes } from "../../components/AceptarSolicitudes/ListadoSolicitudes";
 import { SpinnerLoading } from "../../components/Spinner/SpinnerLoading";
+import MySwal from "sweetalert2";
 
 export const SolicitudesContainer = () => {   
-    const [ solicitudes, setSolicitudes ] = useState([]); 
-    //const [ iSearchedButton, setISearchedButton ] = useState(false);
-    const [ clicked, setClicked ] = useState(0);
-    const [ mounted, setMounted ] = useState();
+    const [ solicitudes, setSolicitudes ] = useState(); 
     const [ spinner, setSpinner] = useState(false);
-    //const [dni, setDni] = useState('')
-    // Cambiar por un 1 y ordenar BD
-    //const [zonaId, setZonaId] = useState('') 
+    const [ hasClicked, setHasClicked ] = useState();
+
+    const fetchSolicitudes = async () => {
+        try{
+            const response = await axios.get("http://localhost:8080/getSolicitudes");
+            console.log(response.data)
+            setSolicitudes(response.data)
+        }
+        catch(e){
+            console.log(e.stack)
+        }
+    }
+
+    const eliminarSolicitud = async () => {
+        MySwal.fire({
+            title: '¿Está seguro que desea rechazar la solicitud?',
+            text: 'No queremos problemas okk?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#198754',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si padre',
+            cancelButtonText: 'Ay, mejor no',
+        }).then( (result) => {
+            if(result.isConfirmed){
+                MySwal.fire({
+                    title:'Eliminado!',
+                    text:'La solicitud fue eliminada con exito',
+                    icon:'success'
+                })
+            }
+        })
+    }
 
     useEffect(()=>{
-        const fetchSolicitudes = async () => {
-            try{
-                const response = await axios.get("http://localhost:8080/getSolicitudes");
-                console.log(response.data)
-                //const [ solicitudes, setSolicitudes ] = useState([]); 
-                setSolicitudes(response.data)
-                //setISearchedButton(false);
-            }
-            catch(e){
-                console.log(e.stack)
-            }
-        }
-
-        if(clicked == 0){
-            fetchSolicitudes();
-            setMounted(true)
-        }
-
-    }, [clicked])
-
-    //const handleZonaSubmit = (event) =>{
-    //    event.preventDefault();
-    //    console.log(event.target.zonaId.value);
-    //}
+        fetchSolicitudes();
+    }, [])
 
     return(
         <>
-        {mounted ?
             <Container className="mt-4">
-                <h1>Solicitudes para la vacuna de la Fiebre Amarilla</h1>
-                <hr/>
+                <h1 className="display-5">Solicitudes para la vacuna de la Fiebre Amarilla</h1>
                 <Row>
-                    <ListadoSolicitudes solicitudes={solicitudes} />
+                    {solicitudes ? <ListadoSolicitudes solicitudes={solicitudes} eliminarSolicitud={eliminarSolicitud} /> : <SpinnerLoading/>}
                 </Row>
-            </Container>  
-        : <SpinnerLoading/>    
-        }  
+            </Container>    
         </>
     )
 }
